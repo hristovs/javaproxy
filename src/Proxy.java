ff7daa0836c2ef67514c3960e4b57cd092badb09
hristovs <hristovs@tcd.ie>
2017-02-21 08:54:22 +0000

    finished

--- src/Proxy.java
+++ src/Proxy.java
@@ -1,73 +1,91 @@
 import java.io.*;
 import java.net.*;
 import java.util.Arrays;
-
-
 public class Proxy {
-private static final String[] blacklist = new String[50];
-private static int index = 0;
-private static final int off = 0; 
+	private static final String[] blacklist = new String[50];
+	private static int index = 0;
+	private static final int off = 0; 
 	public static void blacklist(String stringToBlacklist){
 		blacklist[index] = stringToBlacklist;
 		index++;
 	}
-
+	public static void checkBlacklist(String str, OutputStream toClient){
+		for(int i = 0; i < index; i++){
+			if(str.contains(blacklist[i])){
+				PrintWriter response = new PrintWriter(toClient);
+				response.print("Server is blacklisted");
+				response.flush();
+				System.out.println("blacklisted");
+				System.exit(0); 
+			}
+		}	
+	}
 	public static void main(String[] args) throws IOException{
 		Proxy proxy = new Proxy();
 		blacklist("imgur.com");
-		while(true){
 		proxy.run();
-		}
-	}
-
+	} 
+	
 	public void run(){
-
 		try {
+			final byte[] request = new byte[8196]; // byte buffer into which request is read
+			byte[] reply = new byte[4096];
 
-		
-				ServerSocket serverSocket = new ServerSocket(10000);
-				Socket clientSocket = serverSocket.accept();
+			ServerSocket serverSocket = new ServerSocket(10000);
+			Socket clientSocket;
+			while(true){
+				clientSocket = serverSocket.accept(); 
+				//client streams
+				final InputStream fromClient = clientSocket.getInputStream();
+				final OutputStream toClient = clientSocket.getOutputStream();
+				InetAddress host= InetAddress.getLocalHost();
+				Socket server = new Socket(host, 80); //port 80 
 
-				InputStream incommingIS = clientSocket.getInputStream();
-				OutputStream toClient = clientSocket.getOutputStream();
+			//server streams
+				final InputStream response = server.getInputStream();
+				final OutputStream sendOutput = server.getOutputStream();
 
-				byte[] request = new byte[8196]; // byte buffer into which request is read
-				byte[] reply = new byte[4096];
+				Thread t = new Thread() {
+					public void run() {
+						int len;
+						try {
+							while ((len = fromClient.read(request)) != -1) {
+								String str = new String(request, "UTF-8");
+								System.out.println(str);
+								checkBlacklist(str, toClient);
 
-				int len = incommingIS.read(request); //length of buffered bits
-				
-				String str = new String(request, "UTF-8");
-				System.out.println(str);
-				for(int i = 0; i < blacklist.length; i++){
-					if(str.contains(blacklist[i])){
-				
-					String blacklistResponseString = "<h1>Blacklisted</h1>";
-					byte[] blacklistResponseBytes = blacklistResponseString.getBytes();
-					toClient.write(blacklistResponseBytes, off, blacklistResponseBytes.length );
-					System.out.println("blacklisted");
-					System.exit(0); 
+								sendOutput.write(request,off,len);
+								sendOutput.flush();
+							}
+						} catch (IOException e) {
+						}
+
+
+						try {
+							sendOutput.close();
+						} catch (IOException e) {
+						}
 					}
-				}
+				};
 
-				if(len > 0){
-					//send off to internet
-					Socket iNetSocket = new Socket("localhost", 80); //port 80 yo
-					InputStream response = iNetSocket.getInputStream();
-					OutputStream sendOutput = iNetSocket.getOutputStream();
-					sendOutput.write(request,off,len);
-					sendOutput.flush();
 
-					int bytesback = response.read(reply);
-					toClient.write(reply,off,bytesback);
+				t.start();
+
+				int bytesBack;
+				while ((bytesBack = response.read(reply)) != -1) {
+					toClient.write(reply,off,bytesBack);
 					toClient.flush();
-
 				}
-			          
-		}
 
+
+			}
+
+		}
 		catch(IOException e){
 
 		}
+
+
 	}
 
 }
